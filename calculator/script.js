7cf64e3c1450631d53ff082c64836a3aed4a8790
nehamol31 <nehamolsajeev@gmail.com>
2024-08-20 20:23:07 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,21 @@
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let result = document.getElementById('result').value;
+    document.getElementById('result').value = result.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('result').value);
+        document.getElementById('result').value = result;
+    } catch (error) {
+        document.getElementById('result').value = 'Error';
+    }
+}
